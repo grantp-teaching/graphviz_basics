507097dcc30e7140ad1cfa43f497b43c1b574fdb
Peadar Grant <grantp@dkit.ie>
2021-09-29 13:02:48 +0100

    initial commit

---
+++ system_check.ps1
@@ -0,0 +1,12 @@
+﻿# system check script for graphviz
+
+$CommandToCheck = "dot"
+Write-Host -NoNewline "checking for $CommandToCheck... "
+if ( ! (Get-Command $CommandToCheck -ErrorAction SilentlyContinue ) ) {
+    Write-Error "$CommandToCheck not installed"
+    Return
+}
+Write-Host "ok"
+
+Write-Host "All OK"
+
